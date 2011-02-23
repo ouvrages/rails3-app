ef336fc5d57799d0a8c2bb8f4b0523a3788158bb
Michael Witrant <mike@lepton.fr>
2011-02-23 10:37:39 +0100

    fails when curl fails

--- app.rb
+++ app.rb
@@ -15,7 +15,10 @@ application generators
 
 get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
 get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js", "public/javascripts/jquery-ui.js"
-`curl http://github.com/rails/jquery-ujs/raw/master/src/rails.js -o public/javascripts/rails.js`
+run "curl --silent http://github.com/rails/jquery-ujs/raw/master/src/rails.js -o public/javascripts/rails.js"
+unless File.exists? 'public/javascripts/rails.js'
+  raise "failed to download http://github.com/rails/jquery-ujs/raw/master/src/rails.js with curl"
+end
 
 gsub_file 'config/application.rb', 'config.action_view.javascript_expansions[:defaults] = %w()', 'config.action_view.javascript_expansions[:defaults] = %w(jquery.js jquery-ui.js rails.js)'
 
