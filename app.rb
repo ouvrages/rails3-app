d98560797fde74caf45a02173933cfa21a55c3e8
Michael Witrant <mike@lepton.fr>
2011-03-11 12:50:11 +0100

    version ouvrages_engine

--- app.rb
+++ app.rb
@@ -1,69 +1,26 @@
-gem "factory_girl_rails", ">= 1.0.0", :group => :test
-gem "factory_girl_generator", ">= 0.0.1", :group => [:development, :test]
 gem "haml-rails", ">= 0.3.4"
-gem "rspec-rails", ">= 2.2.1", :group => [:development, :test]
 gem "yaml_db"
+gem 'paperclip'
+gem 'ouvrages-flvtool2', :require => nil
+gem 'rmagick', :require => nil
+gem 'mysql2'
+gem 'jquery-rails', '>= 0.2.6'
+gem "ouvrages_engine", :path => "../ouvrages_engine"
 
-generators = <<-GENERATORS
-
-    config.generators do |g|
-      g.test_framework :rspec, :fixture => true, :views => false
-      g.integration_tool :rspec, :fixture => true, :views => true
-    end
-GENERATORS
-
-application generators
-
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
-get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js", "public/javascripts/jquery-ui.js"
-get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
-
-gsub_file 'config/application.rb', 'config.action_view.javascript_expansions[:defaults] = %w()', 'config.action_view.javascript_expansions[:defaults] = %w(jquery.js jquery-ui.js rails.js)'
-
-layout = <<-LAYOUT
-!!!
-%html
-  %head
-    %title #{app_name.humanize}
-    = stylesheet_link_tag :all
-    = javascript_include_tag :defaults
-    = csrf_meta_tag
-  %body
-    #header
-      %h1 #{app_name.humanize}
-    #content
-      = yield
-    #footer
-LAYOUT
+application "config.i18n.default_locale = :fr"
 
 remove_file "app/views/layouts/application.html.erb"
-create_file "app/views/layouts/application.html.haml", layout
 
 create_file "log/.gitkeep"
 create_file "tmp/.gitkeep"
 
 run "bundle install"
-run "script/rails generate rspec:install"
 
-index = <<-INDEX
-%p Welcome!
-INDEX
-
-run "script/rails generate controller main"
-create_file "app/views/main/index.html.haml", index
-route "root :to => \"main#index\""
 remove_file "public/index.html"
 
-sass = <<-SASS
-@import meyer-reset
-
-SASS
-
-create_file "public/stylesheets/sass/application.sass", sass
-
-get "https://github.com/adamstac/meyer-reset/raw/master/stylesheets/_meyer-reset.sass", "public/stylesheets/sass/_meyer-reset.sass"
+run "script/rails generate ouvrages_engine:install"
 
 git :init
 git :add => "."
-git :commit => "-m 'Generated app with RSpec, Factory Girl, Haml, jQuery, yaml_db and meyer-reset'"
+git :commit => "-m 'Generated app with Ouvrages Engine'"
 
